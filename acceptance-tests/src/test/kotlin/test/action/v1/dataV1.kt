a84ae6463972b857b0f1acdbd6dac77fa7b7ae48
Владимир Овсянников <geniyz@yandex.com>
2023-06-15 12:12:56 +0300

    Приёмочные тесты — подправил порт ktor-приложения и someCreateObj привёт в соответствие отдаваемому стабу

--- acceptance-tests/src/test/kotlin/test/action/v1/dataV1.kt
+++ acceptance-tests/src/test/kotlin/test/action/v1/dataV1.kt
@@ -1,11 +1,12 @@
 package site.geniyz.otus.blackbox.test.action.v1
 
 import site.geniyz.otus.api.v1.models.*
+// import site.geniyz.otus.stubs.*
 
 val debug = ObjDebug(mode = RequestDebugMode.STUB, stub = ObjRequestDebugStubs.SUCCESS)
 
 val someCreateObj = ObjCreateObject(
-    name    = "Панграмма1",
-    content = "Чушь: гид вёз кэб цапф, юный жмот съел хрящ.",
+    name    = "Текст1",
+    content = "Какой-то тест",
     objType = ObjType.TEXT,
 )
